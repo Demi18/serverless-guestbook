ccd6218e0711823ad63a740da502f71e98e36d13
Demi18 <41676374+Demi18@users.noreply.github.com>
2018-08-03 13:59:29 +0800

    Update prepare-documents.js

--- actions/prepare-documents.js
+++ actions/prepare-documents.js
@@ -2,16 +2,16 @@
  * Prepare the guestbook entry to be persisted
  */
 function main(params) {
-  if (!params.name || !params.comment) {
-    return Promise.reject({error: 'no name or comment'});
+  if (!params.city || !params.longitude) {
+    return Promise.reject({error: 'no city or longitude'});
   }
 
 	return {
     doc: {
       createdAt: new Date(),
-	    name: params.name,
-	    email: params.email,
-	    comment: params.comment,
+	    city: params.city,
+	    latitude: params.latitude,
+	    longitude: params.longitude,
 	  }
   };
 }
